9ec554c4493ac81f8ed4ea8e8b152218f54ca5b7
alistairholmes <alistairholmes95@gmail.com>
2017-10-02 19:52:56 +0200

    Changed my approach and decided to go with the fragments first to save time.
    Made a custom Info object which will store the information required.

---
+++ app/src/main/java/com/example/android/bulawayo_city_guide_app/Info.java
@@ -0,0 +1,77 @@
+package com.example.android.bulawayo_city_guide_app;
+
+/**
+ *  {@link Info} Is a custom object that contains information about attractions in a city.
+ *  Information includes a Title, a Description translation, an Image for that attraction,
+ *  Address, Operating Hours and a Star Rating.
+ */
+
+public class Info {
+
+    // Title name for the attraction
+    private String mTitle;
+
+    // Description for the attraction
+    private String mDescription;
+
+    // ImageResource ID for attraction
+    private int mImageResourceID;
+
+    // Location of attraction
+    private String mAddress;
+
+    // Operating Hours of attraction
+    private String mOperatingHours;
+
+    // Star Rating of attraction
+    private double mStarRating;
+
+    /**
+     * Create a new Info object.
+     *
+     * @param title is the name of the attraction.
+     * @param description is the description of the attraction
+     * @param imageResourceID id of the image resource
+     * @param address location of attraction
+     * @param operatingHours operating hours of attraction
+     * @param starRating rating of attraction
+     */
+    public Info (String title, String description, int imageResourceID, String address, String operatingHours, double starRating) {
+        mTitle = title;
+        mDescription = description;
+        mImageResourceID = imageResourceID;
+        mAddress = address;
+        mOperatingHours = operatingHours;
+        mStarRating = starRating;
+    }
+
+    // Get the title of attraction.
+    public String getTitle() {
+        return mTitle;
+    }
+
+    // Get description of attraction
+    public String getDescription() {
+        return mDescription;
+    }
+
+    // Return imageResource
+    public int getImageResourceID() {
+        return mImageResourceID;
+    }
+
+    // Get address of attraction
+    public String getAddress() {
+        return mAddress;
+    }
+
+    // Get operating hours
+    public String getOperatingHours() {
+        return mOperatingHours;
+    }
+
+    // Get star rating
+    public double getStarRating() {
+        return mStarRating;
+    }
+}
